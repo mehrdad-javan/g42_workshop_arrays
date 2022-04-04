53f7bb88cd28a44268955a9c08f14094eaf8e246
Mehrdad Javan <mehrdad.javan@adapta.se>
2022-04-04 09:18:06 +0200

    Create NameRepository Class + implement getSize method

---
+++ src/main/java/se/lexicon/NameRepository.java
@@ -0,0 +1,25 @@
+package se.lexicon;
+
+
+// Class: A class is a logical template or blueprint
+// to create objects for sharing the common properties and methods
+public class NameRepository {
+
+  // Fields or Variables
+  // A variable is a container that stores values
+  // Array is a data structure or group or collection of elements with the same data types
+
+
+  private static String[] names = new String[0];
+
+  // Method is a block of code which only runs when it is called
+  // it is a collection of statements that grouped together to perform an operation
+
+
+  public static int getSize(){
+    return names.length;
+  }
+
+
+
+}
