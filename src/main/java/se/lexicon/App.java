5d07bf9396e46acaab53ae6501e31ce28d4795ad
Mehrdad Javan <mehrdad.javan@adapta.se>
2022-04-04 09:30:45 +0200

    add java doc + implement setNames

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -2,12 +2,13 @@ package se.lexicon;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static void main(String[] args) {
+      System.out.println(NameRepository.getSize());
+      String [] names = {"Erik Svensson", "Mehrdad Javan"};
+      NameRepository.setNames(names);
+    System.out.println(NameRepository.getSize());
+
+  }
 }
--- src/main/java/se/lexicon/NameRepository.java
+++ src/main/java/se/lexicon/NameRepository.java
@@ -3,6 +3,13 @@ package se.lexicon;
 
 // Class: A class is a logical template or blueprint
 // to create objects for sharing the common properties and methods
+
+/**
+ * Stores names in a String array
+ * @author Mehrdad Javan
+ * @version 1.0
+ * @since 2022-04-04
+ */
 public class NameRepository {
 
   // Fields or Variables
@@ -15,11 +22,25 @@ public class NameRepository {
   // Method is a block of code which only runs when it is called
   // it is a collection of statements that grouped together to perform an operation
 
-
+  /**
+   * Returns number of element in the array
+   *
+   * @return int This returns length of elements in names array
+   */
   public static int getSize(){
     return names.length;
   }
 
+  /**
+   * Sends in an array that set the private static array.
+   * This should replace all existing names.
+   *
+   * @param names This is an array of names
+   */
+  public static void setNames(String[] names) {
+    // use Arrays.copyOf()
+    NameRepository.names = names;
+  }
 
 
 }
